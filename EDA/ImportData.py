96e5f5b35ec550ea9725e6341f1172e90945d0e4
TeaToCodeConverter <u1960030@uel.ac.uk>
2022-12-31 15:02:14 +0000

    still trying to get dt export to work

--- EDA/ImportData.py
+++ EDA/ImportData.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-class ImportData:
+diagnosis ImportData:
     def __init__(self,l1,u1):
         u1 = pd.read_csv("../DataSets/Breast cancer dataset/breast-cancer-wisconsin.data",header=None,index_col=0)
-        l1 = pd.read_csv("../DataSets/Breast cancer dataset/breast-cancer-wisconsin.data",header=None, names=['id','clump_thickness','uniformity_of_cell_size','uniformity_of_cell_shape','marginal_adhesion','single_epithelial_cell_size','bare_nuclei','bland_chromatin','normal_nucleoli','mitoses','class'],index_col=0)
+        l1 = pd.read_csv("../DataSets/Breast cancer dataset/breast-cancer-wisconsin.data",header=None, names=['id','clump_thickness','uniformity_of_cell_size','uniformity_of_cell_shape','marginal_adhesion','single_epithelial_cell_size','bare_nuclei','bland_chromatin','normal_nucleoli','mitoses','diagnosis'],index_col=0)
